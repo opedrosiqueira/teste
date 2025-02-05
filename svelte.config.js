4f768e25578aac6e4ed128fb7eeb7f56d25978af
pedrosiqueira <pedro.siqueira@ifms.edu.br>
2025-02-05 18:43:03 -0400

    link quebrado com fallback pra testar

--- svelte.config.js
+++ svelte.config.js
@@ -2,7 +2,7 @@ import adapter from '@sveltejs/adapter-static';
 
 export default {
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({ fallback: '404.html' }),
 		paths: { base: '/teste' }
 	}
 };
