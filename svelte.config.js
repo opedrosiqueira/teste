c964bf023e4af397d9f576c7a213a0f57376b14e
pedrosiqueira <pedro.siqueira@ifms.edu.br>
2025-02-05 15:21:17 -0400

    first commit

---
+++ svelte.config.js
@@ -0,0 +1,8 @@
+import adapter from '@sveltejs/adapter-static';
+
+export default {
+	kit: {
+		adapter: adapter({ fallback: '404.html' }),
+		paths: { base: process.argv.includes('dev') ? '' : process.env.BASE_PATH }
+	}
+};
