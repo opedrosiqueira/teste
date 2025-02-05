90bed82cec76183a23f1e1170959ea24535237df
pedrosiqueira <pedro.siqueira@ifms.edu.br>
2025-02-05 18:40:15 -0400

    sem falback, o que acontece?

--- svelte.config.js
+++ svelte.config.js
@@ -2,7 +2,7 @@ import adapter from '@sveltejs/adapter-static';
 
 export default {
 	kit: {
-		adapter: adapter({ fallback: '404.html' }),
+		adapter: adapter(),
 		paths: { base: '/teste' }
 	}
 };
